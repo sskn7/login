76841365a271f3219d4ba77367217cecbb36390e
kana sasazawa <kana.yellow7@gmail.com>
2025-06-10 22:20:19 +0900

    docs: :heavy_plus_sign #LogunDAOを追加

--- src/com/diworksdev/login/dto/LoginDTO.java
+++ src/com/diworksdev/login/dto/LoginDTO.java
@@ -1,30 +1,37 @@
 package com.diworksdev.login.dto;
 
 public class LoginDTO {
-	public class LoginDTO {
 		private int id;
+		//フィールド変数を宣言
 		private String name;
-		private Srting password;
-		public int getID() {
+		private String password;
+
+	//フィールド変数に対応したgetterとsetterを定義する
+	public int getID() {
 			return id;
-		}
 	}
 
 	public void setID(int id) {
 		this.id=id;
 	}
-	//DAOクラスに呼び出され、引数として受け取ったテーブルの値を
-	//自身のnameフィールドに格納
-	public String setName() {
+
+	//Actionクラスから呼び出され、nameフィールドの値をActionに渡す
+	public String getName() {
 		return name;
 	}
-	//Actionクラスから呼び出され、passwordフィールドの値をActionni渡す
+
+	//DAOクラスに呼び出され、引数として受け取ったテーブルの値を
+	//自身のnameフィールドに格納
+	public void setName(String name) {
+		this.name = name;
+	}
+	//Actionクラスから呼び出され、passwordフィールドの値をActionに渡す
 	public String getPassord () {
 		return password;
 	}
 	//DAOクラスから呼び出され、引数として受け取ったテーブルの値を
 	//自身のPasswordフィールドに格納する
 	public void setPassword(String password) {
-		this.name=name;
+		this.password = password;
 	}
 }
